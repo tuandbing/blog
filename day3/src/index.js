6fda4be441a2ebf31b71e9cb57bb75efbf925c70
tuandbing <1873656423@qq.com>
2023-07-13 15:51:12 +0800

    修改

--- day3/src/index.js
+++ day3/src/index.js
@@ -1,1 +1,12 @@
-console.log('hello world')
+// 引入sass
+const path = require('path')
+const sass = require('sass');
+
+// 构造文件路径
+const filePath = sass.compile(path.resolve(__dirname, './index.scss'));
+
+// 编译
+const result = sass.compile(filePath)
+
+// 打印结果
+console.log(result.css)
