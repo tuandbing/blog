fd345fd5e306295aa975016534163ab1e2b50992
tuandbing <1873656423@qq.com>
2023-07-16 11:45:14 +0800

    修改

---
+++ day4/test.js
@@ -0,0 +1,15 @@
+function getUserInfo() {
+    // TODO
+}
+
+// 关键字
+// var typeof let
+
+//变量声明
+// 如果声明的类型时对象，直接const
+const value_const = 3;
+let value_let = 2;
+
+// 这种方法基本不用
+var value_var = 1;
+valueNoKeyWords = 4;
