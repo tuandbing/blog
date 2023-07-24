2960a189320a942dfaf4c4e39089b9b457fd7f96
tuandbing <1873656423@qq.com>
2023-07-24 21:07:08 +0800

    添加

--- day5/test03.js
+++ day5/test03.js
@@ -14,3 +14,62 @@ function unique(arr) {
 }
 var demo = unique(['c', 'b', 'a', 's', 'a', 'c', 's', 'v', 'b']);
 console.log(demo);
+
+// 下面是一个简单的基本包装类型的例子
+var str = 'andy';
+console.log(str.length);
+
+// 其实质是
+// (1)把简单数据类型包装为复杂数据类型
+var temp = new String('andy');
+// (2)把临时变量的值给str
+str = temp;
+//(3)销毁这个临时变量
+temp = null;
+
+// 统计一个字符串中某个字符出现的次数，以及对应的下标
+var str = 'abcabsakhaskdbaskdjxbkasdx';
+var index = str.indexOf('b');
+while (index !== -1) {
+    console.log(index);
+    index = str.indexOf('b', index + 1);
+}
+
+
+o = {
+
+}
+for (var i = 0; i < str.length; i++) {
+    var chars = str.charAt(i);
+    if (o[chars]) {
+        o[chars]++;
+    } else {
+        o[chars] = 1;
+    }
+}
+console.log(o);
+var max = 0;
+var ch = '';
+for (var k in o) {
+    if (max < o[k]) {
+        max = o[k];
+        ch = k;
+    }
+}
+console.log(max);
+console.log(ch);
+
+function count(o, a) {
+    if (o[a]) {
+        o[a]++;
+    } else {
+        o[a] = 1;
+    }
+}
+var j = {
+
+}
+for (var i = 0; i < str.length; i++) {
+    count(j, str.charAt(i));
+}
+console.log(j);
